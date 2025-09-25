7a0b7629099e77d06ac56e7f4defa31b506e487b
MeenugaRaghavendra1994 <meenuga.raghavendra@orchidsintl.edu.in>
2025-09-25 17:35:54 +0000

    Commit

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 # Create a SQLite database (file based)
 DATABASE_URL = "postgresql+psycopg2://username:password@localhost:5432/mydb"
@@ -10,16 +10,17 @@ engine = create_engine(DATABASE_URL)
 # Function to create table if not exists
 def init_db():
     with engine.connect() as conn:
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS school_books (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Zone TEXT,
-            Grade TEXT,
-            SKU TEXT,
-            Book_Name TEXT,
-            Book_Category TEXT,
-            Qty INTEGER,
-            Selling_Price REAL,
-            Cost_Price REAL
-        );
-        """)
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS school_books (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Zone TEXT,
+                Grade TEXT,
+                SKU TEXT,
+                Book_Name TEXT,
+                Book_Category TEXT,
+                Qty INTEGER,
+                Selling_Price REAL,
+                Cost_Price REAL
+            )
+        """))
+        conn.commit()
